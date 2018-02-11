eb93442ed0b9866ab770aa05b2fc6bb4152b3e19
LuisCasas <sc.luis@gmail.com>
2018-02-11 18:47:41 +0000

    Fit App: Finish application functionality

--- app/src/main/java/com/luiscasas/flaborfit/DetailsActivity.java
+++ app/src/main/java/com/luiscasas/flaborfit/DetailsActivity.java
@@ -1,8 +1,10 @@
 package com.luiscasas.flaborfit;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class DetailsActivity extends AppCompatActivity {
@@ -12,7 +14,24 @@ public class DetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
-        TextView exerciseView = (TextView)findViewById(R.id.exerciseTitle);
+        TextView exerciseText = (TextView)findViewById(R.id.exerciseTitle);
         ImageView exerciseImg = (ImageView)findViewById(R.id.exerciseImg);
+        LinearLayout mainBG = (LinearLayout)findViewById(R.id.mainBG);
+
+        String exerciseTitle = getIntent().getStringExtra(MainActivity.EXTRA_ITEM_TITLE);
+
+        exerciseText.setText(exerciseTitle);
+
+        if(exerciseTitle.equalsIgnoreCase(MainActivity.EXERCISE_WEIGHTS)){
+            exerciseImg.setImageDrawable(getResources().getDrawable(R.drawable.weight, getApplicationContext().getTheme()));
+            mainBG.setBackgroundColor(Color.parseColor("#2ca5f5"));
+        } else  if(exerciseTitle.equalsIgnoreCase(MainActivity.EXERCISE_YOGA)){
+            exerciseImg.setImageDrawable(getResources().getDrawable(R.drawable.lotus, getApplicationContext().getTheme()));
+            mainBG.setBackgroundColor(Color.parseColor("#916bcd"));
+        } else  if(exerciseTitle.equalsIgnoreCase(MainActivity.EXERCISE_CARDIO)){
+            exerciseImg.setImageDrawable(getResources().getDrawable(R.drawable.heart, getApplicationContext().getTheme()));
+            mainBG.setBackgroundColor(Color.parseColor("#52ad56"));
+        }
+
     }
 }
